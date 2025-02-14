31c1820b5ad9c8a0a882cdb81ad7f9ae04d0915c
Nwankwor Benjamin <Benjaminnwankwor2109@gmail.com>
2025-02-14 13:34:11 +0100

    Code additions and testing

---
+++ script.js
@@ -0,0 +1,12 @@
+var spanElement = document.getElementById('animated-text');
+
+spanElement.addEventListener("mouseleave", AddAnimation);
+
+function AddAnimation(){
+    console.log("Hello World!");
+    spanElement.classList.add("bouncingAnimation");
+
+    setTimeout(() => {
+        spanElement.classList.remove("bouncingAnimation");
+    }, 5000);
+}
